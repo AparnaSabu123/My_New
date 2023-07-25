6b470a79851ba35c218f5a99ba37421875eff4e3
Aparna <Aparna@LAPTOP-5AU7GJHM>
2023-07-25 14:33:41 +0530

    second commit

---
+++ My_New/src/main/java/newsample/ConstructorSsample.java
@@ -0,0 +1,16 @@
+package newsample;
+
+public class ConstructorSsample {
+
+	ConstructorSsample(String name)
+	{
+		System.out.println(name);
+	}
+	
+	public static void main(String[] args) {
+		ConstructorSsample cs=new ConstructorSsample("Aparna");
+		// TODO Auto-generated method stub
+
+	}
+
+}
