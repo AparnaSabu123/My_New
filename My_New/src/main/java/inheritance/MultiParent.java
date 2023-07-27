68af857abe56352414d99f08fe74ba6c2d8caf30
Aparna <Aparna@LAPTOP-5AU7GJHM>
2023-07-27 08:59:00 +0530

    multiparent push

---
+++ My_New/src/main/java/inheritance/MultiParent.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+public class MultiParent extends MultiGrand {
+	int c=100;
+	int d=20;
+	public void multiple()
+	{
+	
+	int a=10;
+	int b=20;
+	System.out.println(name);
+	System.out.println(a*b);
+	}
+
+}
