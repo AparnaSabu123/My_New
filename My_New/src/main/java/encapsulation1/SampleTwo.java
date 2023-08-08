16eae836dc6cf35af3566508ef586ac4f6d8c8ae
Aparna <Aparna@LAPTOP-5AU7GJHM>
2023-08-08 07:59:35 +0530

    encapsulation

---
+++ My_New/src/main/java/encapsulation1/SampleTwo.java
@@ -0,0 +1,16 @@
+package encapsulation1;
+
+import encapsulation.SampleOne;
+
+public class SampleTwo {
+
+	public static void main(String[] args) {
+		
+		SampleOne obj=new SampleOne();  //aggregation
+		obj.setter("Aparna", 30);
+		obj.getter();
+		// TODO Auto-generated method stub
+
+	}
+
+}
